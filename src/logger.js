5ba4ac0704bc0c6c2785ba665e273fb19ee5c066
MatthewNorton <mattnorton.codes@gmail.com>
2023-08-10 23:17:01 -0400

    optimized initial logger removed async in function

--- src/logger.js
+++ src/logger.js
@@ -1,59 +1,61 @@
 const ENABLE_LOGGING = true; // Global flag to enable or disable logging
 
 function logger(description, func, enableLogging = true) {
-  const emojis = {
-    "Variables": "💾",
-    "Arrays": "🔢",
-    "Objects": "🏗",
-    "Object Creation and Modification": "🏗️",
-    "Property and Key Access": "🔑",
-    "Object Modification": "🛠️",
-    "Object Testing": "🔍",
-    "Object Iteration": "🔄",
-    "Functions": "🧩",
-    "Loops": "🔄",
-    "Conditional": "❓",
-    "Promises": "🤞",
-    "Fetch": "📡",
-    "Classes": "🎓",
-    "DOM": "🔧",
-    "Event Handling": "🎯",
-    "Debugging": "🐛",
-    "Testing": "✔️",
-    "Comments": "💬",
-    "Console": "📝",
-    "Math and Numbers": "➕➖✖️➗",
-  };
-  if (!enableLogging) {
-    return func; // Return the original function if logging is disabled
-  }
-  const emoji = emojis[description] || "👀";
-  const functionSource = func.toString();
-  // line number retrieval
-  const lineNumber = new Error().stack.match(/(?:\((.*?)\)| (.*?):)(\d+):/)[3];
-  return async function (...args) {
-    console.group(emoji + " " + description); // Start a group
-    console.log('🚀 Input:', args);
-    console.log(`Function (line ${lineNumber}):`, functionSource);
-
-    // async functions
-    let result;
-    if (func.constructor.name === "AsyncFunction") {
-      try {
-        result = await func(...args);
-        console.log('Promise State: Resolved');
-      } catch (e) {
-        console.log('Promise State: Rejected');
-        console.error(e);
-      }
-    } else {
-      result = func(...args);
+    const emojis = {
+      "Variables": "💾",
+      "Arrays": "🔢",
+      "Objects": "🏗",
+      "Object Creation and Modification": "🏗️",
+      "Property and Key Access": "🔑",
+      "Object Modification": "🛠️",
+      "Object Testing": "🔍",
+      "Object Iteration": "🔄",
+      "Functions": "🧩",
+      "Loops": "🔄",
+      "Conditional": "❓",
+      "Promises": "🤞",
+      "Fetch": "📡",
+      "Classes": "🎓",
+      "DOM": "🔧",
+      "Event Handling": "🎯",
+      "Debugging": "🐛",
+      "Testing": "✔️",
+      "Comments": "💬",
+      "Console": "📝",
+      "Math and Numbers": "➕➖✖️➗",
+    };
+    if (!enableLogging) {
+      return func; // Return the original function if logging is disabled
     }
-
-    console.log('🪐 Output:', result);
-    console.groupEnd(); // End the group
-    return result;
-  };
-}
+    const emoji = emojis[description] || "👀";
+  
+    return function (...args) {
+      try {
+        console.group(emoji + " " + description); // Start a group
+        console.log('🚀 Input:', args);
+  
+        // Get the original function source code
+        const functionSource = func.toString();
+  
+        // Get the line number where the logger function is called
+        const error = new Error();
+        const stackLine = error.stack.split('\n')[2];
+        const lineNumber = stackLine.match(/:(\d+):\d+/)[1];
+  
+        console.log(`Function (line ${lineNumber}):`, functionSource);
+  
+        const result = func(...args);
+  
+        console.log('🪐 Output:', result);
+        console.groupEnd(); // End the group
+  
+        return result;
+      } catch (error) {
+        console.error('🔥 An error occurred:', error);
+        console.groupEnd(); // End the group even in case of error
+        throw error;
+      }
+    };
+  }
 
 module.exports = logger
